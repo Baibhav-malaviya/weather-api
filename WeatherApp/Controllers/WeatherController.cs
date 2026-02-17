c38b9f75295a9d3ede4ad9e24131c98558119f7c
Baibhav Kumar <baibhav_kumar@epam.com>
2026-02-17 21:41:26 +0530

    We have done without db, now we will implement the sql server

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Services.Interfaces;
+
+namespace WeatherApiJwt.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+//[Authorize]
+public class WeatherController : ControllerBase
+{
+    private readonly IWeatherService _weatherService;
+
+    public WeatherController(IWeatherService weatherService)
+    {
+        _weatherService = weatherService;
+    }
+
+    [HttpGet("{city}")]
+    public async Task<IActionResult> GetWeather(string city)
+    {
+        var result = await _weatherService.GetWeatherAsync(city);
+        return Ok(result);
+    }
+}
---
+++ WeatherApp/Models/User.Model.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
