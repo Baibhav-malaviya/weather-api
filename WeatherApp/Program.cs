714569693ef5f525840fe06925a206f2fc7e5c68
Baibhav Kumar <baibhav_kumar@epam.com>
2026-02-22 14:52:03 +0530

    Dockerfile added and changed the local db to mssql image

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -70,12 +70,21 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
 app.UseCors("AllowAngularApp");
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
